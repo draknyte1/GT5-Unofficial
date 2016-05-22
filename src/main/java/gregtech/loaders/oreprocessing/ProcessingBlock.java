d2ac1d5e91e6e59c1fd547c1212bd6a9a71bd604
draknyte1 <draknyte1@hotmail.com>
2016-05-22 17:00:43 +1000

    Feel free to merge @Bogdan-G Removed debug comments, fixed Gem processing exploits. Can be changed, just need to get a public opinion on it first. Some people think gemBlocks -> 9x gems is okay, but I do not.

--- src/main/java/gregtech/loaders/oreprocessing/ProcessingBlock.java
+++ src/main/java/gregtech/loaders/oreprocessing/ProcessingBlock.java
@@ -24,8 +24,6 @@ public class ProcessingBlock implements gregtech.api.interfaces.IOreRecipeRegist
 	public void registerOre(OrePrefixes aPrefix, Materials aMaterial, String aOreDictName, String aModName, ItemStack aStack) {
 		GT_Values.RA.addCutterRecipe(GT_Utility.copyAmount(1L, new Object[]{aStack}), GT_OreDictUnificator.get(OrePrefixes.plate, aMaterial, 9L), null, (int) Math.max(aMaterial.getMass() * 10L, 1L), 30);
 
-		System.err.println("[GREGTECH 5 DEBUG] - Working with Material: "+aMaterial.name());
-
 		ItemStack tStack1 = GT_OreDictUnificator.get(OrePrefixes.ingot, aMaterial, 1L);
 		ItemStack tStack2 = GT_OreDictUnificator.get(OrePrefixes.gem, aMaterial, 1L);
 		ItemStack tStack3 = GT_OreDictUnificator.get(OrePrefixes.dust, aMaterial, 1L);
@@ -33,15 +31,12 @@ public class ProcessingBlock implements gregtech.api.interfaces.IOreRecipeRegist
 		GT_ModHandler.removeRecipe(new ItemStack[]{GT_Utility.copyAmount(1L, new Object[]{aStack})});
 
 		if (tStack1 != null) {
-			System.err.println("[GREGTECH 5 DEBUG] - Removing recipes for Ingot of Material: "+aMaterial.name());
 			GT_ModHandler.removeRecipe(new ItemStack[]{tStack1, tStack1, tStack1, tStack1, tStack1, tStack1, tStack1, tStack1, tStack1});
 		}
 		if (tStack2 != null) {
-			System.err.println("[GREGTECH 5 DEBUG] - Removing recipes for Gem of Material: "+aMaterial.name());
 			GT_ModHandler.removeRecipe(new ItemStack[]{tStack2, tStack2, tStack2, tStack2, tStack2, tStack2, tStack2, tStack2, tStack2});
 		}
 		if (tStack3 != null) {
-			System.err.println("[GREGTECH 5 DEBUG] - Removing recipes for Dust of Material: "+aMaterial.name());
 			GT_ModHandler.removeRecipe(new ItemStack[]{tStack3, tStack3, tStack3, tStack3, tStack3, tStack3, tStack3, tStack3, tStack3});
 		}
 		if (aMaterial.mStandardMoltenFluid != null) {
@@ -49,64 +44,52 @@ public class ProcessingBlock implements gregtech.api.interfaces.IOreRecipeRegist
 		}
 		if (GregTech_API.sRecipeFile.get(ConfigCategories.Recipes.storageblockcrafting, OrePrefixes.block.get(aMaterial).toString(), false)) {
 			if ((tStack1 == null) && (tStack2 == null) && (tStack3 != null)){
-				System.err.println("[GREGTECH 5 DEBUG] Is tStack1 null?  "+tStack1.toString()+" | Is tStack2 null? "+tStack2.toString()+" | Is tStack3 valid? "+tStack3.toString());
 				GT_ModHandler.addCraftingRecipe(GT_OreDictUnificator.get(OrePrefixes.block, aMaterial, 1L), new Object[]{"XXX", "XXX", "XXX", Character.valueOf('X'), OrePrefixes.dust.get(aMaterial)});
 			}
 			if (tStack2 != null){ 
-				System.err.println("[GREGTECH 5 DEBUG] Is tStack1 null?  "+tStack1.toString()+" | Is tStack2 valid? "+tStack2.toString()+" | Is tStack3 null? "+tStack3.toString());
 				GT_ModHandler.addCraftingRecipe(GT_OreDictUnificator.get(OrePrefixes.block, aMaterial, 1L), new Object[]{"XXX", "XXX", "XXX", Character.valueOf('X'), OrePrefixes.gem.get(aMaterial)});
 			}
 			if (tStack1 != null) {
-				System.err.println("[GREGTECH 5 DEBUG] Is tStack1 valid?  "+tStack1.toString()+" | Is tStack2 null? "+tStack2.toString()+" | Is tStack3 null? "+tStack3.toString());
 				GT_ModHandler.addCraftingRecipe(GT_OreDictUnificator.get(OrePrefixes.block, aMaterial, 1L), new Object[]{"XXX", "XXX", "XXX", Character.valueOf('X'), OrePrefixes.ingot.get(aMaterial)});
 			}
 		}
 
 		if (aMaterial.contains(SubTag.CRYSTAL) && !aMaterial.contains(SubTag.METAL)){
-			System.err.println("[GREGTECH 5 DEBUG] THIS IS A GEM MATERIAL, APPARENTLY.");
 			isGem = true;
 		}
 		else {
-			System.err.println("[GREGTECH 5 DEBUG] THIS IS NOT A GEM MATERIAL, APPARENTLY.");
 			isGem = false;
 		}
-		//Ingots
+		//Ingot
 		if (tStack1 != null) {
-			System.err.println("[GREGTECH 5 DEBUG] Output for tStack1.stackSize = 9");
 			tStack1.stackSize = 9;
 		}
-		//Gems
+		//Gem
 		if (tStack2 != null && !isGem) {
-			System.err.println("[GREGTECH 5 DEBUG] Output for tStack2.stackSize = 9");
 			tStack2.stackSize = 9;
 		}
 		else if (tStack2 != null && isGem) {
-			System.err.println("[GREGTECH 5 DEBUG] Output for tStack2.stackSize = 0");
 			tStack2.stackSize = 0;
 		}
-		//Dusts
+		//Dust
 		if (tStack3 != null && !isGem) {
-			System.err.println("[GREGTECH 5 DEBUG] Output for tStack3.stackSize = 9");
 			tStack3.stackSize = 9;
 		}
 		else if (tStack3 != null && isGem) {
-			System.err.println("[GREGTECH 5 DEBUG] Output for tStack3.stackSize = 0");
 			tStack3.stackSize = 0;
 		}
 
 		//Gems in FORGE HAMMER
 		if (tStack2 != null && !isGem) {
-			System.err.println("[GREGTECH 5 DEBUG] Output for tStack2.stackSize = 9 | FORGE HAMMER RECIPE [metalBlock->9x ingots]");
 			tStack2.stackSize = 9;
 			GT_Values.RA.addForgeHammerRecipe(aStack, tStack2, 100, 24);
 		}
 		else if (tStack2 != null && isGem) {
-			System.err.println("[GREGTECH 5 DEBUG] Output for tStack2.stackSize = 9 | FORGE HAMMER RECIPE [gemBlock->9x gem]");
 			tStack2.stackSize = 9;
 			GT_Values.RA.addForgeHammerRecipe(aStack, tStack2, 100, 24);
 			tStack2.stackSize = 0;
 		}
-		
+
 
 		if (GregTech_API.sRecipeFile.get(ConfigCategories.Recipes.storageblockdecrafting, OrePrefixes.block.get(aMaterial).toString(), tStack2 != null)) {
 			if (tStack3 != null)
