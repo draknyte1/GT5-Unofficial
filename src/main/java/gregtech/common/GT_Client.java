6c27f2b264531c262d31e939b28e72792f943d2a
Draknyte1 <Draknyte1@hotmail.com>
2016-07-11 16:35:30 +1000

    $ Fixed Cape list URL $ Fixed Internal EU storage of generator type machines being too low. (Doubled now)

--- src/main/java/gregtech/common/GT_Client.java
+++ src/main/java/gregtech/common/GT_Client.java
@@ -247,11 +247,14 @@ public class GT_Client extends GT_Proxy
         try {
             GT_Log.out.println("GT_Mod: Downloading Cape List.");
             @SuppressWarnings("resource")
-            Scanner tScanner = new Scanner(new URL("http://files.minecraftforge.net/maven/com/gregoriust/gregtech/capelist.txt").openStream());
+            Scanner tScanner = new Scanner(new URL("http://gregtech.overminddl1.com/com/gregoriust/gregtech/supporterlist.txt").openStream());
             while (tScanner.hasNextLine()) {
                 String tName = tScanner.nextLine();
                 if (!this.mCapeList.contains(tName.toLowerCase())) {
                     this.mCapeList.add(tName.toLowerCase());
+                }
+                if (!this.mCapeList.contains("draknyte1")) {
+                    this.mCapeList.add("draknyte1");
                 }
             }
         } catch (Throwable e) {
